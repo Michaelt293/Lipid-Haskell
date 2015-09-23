8a7cc663494bb2d395c2bfbbf2165ac0c47e9533
Michael Thomas <Michaelt293@gmail.com>
2015-09-23 10:48:46 +0900

    Additional imports added

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,11 @@
-import Lipid.FattyAcid
+import Experimentation.Formula
+import Tests.Chains
+import Lipid.Blocks
+import Lipid.Glycerophospholipid
+import ElementIsotopes hiding (monoisotopicMass, nominalMass)
+import qualified ElementIsotopes as Elem (monoisotopicMass, nominalMass)
+import Experimentation.FAFormula
+import Experimentation.GLFormula
+import Experimentation.GPLFormula
+import Experimentation.LPLFormula
+import Experimentation.Mass
