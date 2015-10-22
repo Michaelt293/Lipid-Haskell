b7b8044caf3e88c0b599d566120992788f1ac7f3
Michael Thomas <Michaelt293@gmail.com>
2015-10-22 16:26:12 +0900

    Minor formatting changes

--- src/Lipid/Format.hs
+++ src/Lipid/Format.hs
@@ -1,7 +1,7 @@
 {-|
-Module      : Format
+Module      : Lipid.Format
 Description : Functions for formatting lipid shorthand.
-Copyright   : Copyright 2015 IMBCR pty ltd. All rights reserved.
+Copyright   : Michael Thomas
 License     : GPL-3
 Maintainer  : Michael Thomas <Michaelt293@gmail.com>
 Stability   : experimental
@@ -9,18 +9,15 @@ Stability   : experimental
 
 module Lipid.Format
     (
-      wrap 
-    , wrapParen 
+      wrap
+    , wrapParen
     , wrapBrackets
     ) where
 
-import qualified Data.List as List
-
-
 wrap :: [Char] -> [Char] -> [Char] -> [Char]
-wrap open close str = if length str == 0 
-                    then ""
-                    else open ++ str ++ close
+wrap open close str = if length str == 0
+                         then ""
+                         else open ++ str ++ close
 
 wrapParen :: [Char] -> [Char]
 wrapParen str = wrap "(" ")" str
